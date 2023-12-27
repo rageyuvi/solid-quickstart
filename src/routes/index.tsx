c1f9ee305f145c5819f7c24ceb78364875853862
rageyuvi <154968114+rageyuvi@users.noreply.github.com>
2023-12-27 14:50:23 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,16 +4,12 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Yuvi Soni</Title>
+      <h1>ルフィ先生</h1>
+  
+      <p><a href="" target="_blank">USELESS-BUTTON</a></p>
+      
       <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
     </main>
   );
 }
